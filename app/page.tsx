73af3000b5b43ea5a6b15ebd9bdd601bb4b1e25a
Shvain <takuto3987@gmail.com>
2024-10-09 15:49:27 +0900

    　デザイン調整

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import React, {useCallback, useState} from "react";
 import {useDropzone} from 'react-dropzone';
 import Image from 'next/image';
+import { CloudUpload, Check, MediaImage } from "iconoir-react";
 
 function MyDropzone() {
   const [uploadedImage, setUploadedImage] = useState<File | null>(null);
@@ -57,11 +58,11 @@ function MyDropzone() {
   };
 
   return (
-    <div className="w-full max-w-4xl flex flex-col items-center">
+    <div className="w-full max-w-4xl flex flex-col items-center isolate">
       <div
         {...getRootProps()}
         className={`border-dashed border-4 p-10 w-full h-96 flex justify-center items-center text-center rounded-xl 
-        ${isDragActive ? "border-blue-500 bg-blue-50" : "border-orange-300"}`}
+        ${isDragActive ? "border-blue-500 bg-blue-50" : "border-white"}`}
       >
         <input {...getInputProps()} />
         {
@@ -71,7 +72,7 @@ function MyDropzone() {
             isDragActive ? (
               <p className="text-blue-500 font-bold">ファイルをここにドロップしてください...</p>
             ) : (
-              <p className="text-gray-600 font-semibold">ファイルをドラッグ＆ドロップするか、クリックして選択してください</p>
+              <p className="text-gray-300 font-semibold">ファイルをドラッグ＆ドロップするか、クリックして選択してください</p>
             )
           )
         }
@@ -81,14 +82,14 @@ function MyDropzone() {
         {uploadedImage && (
             <>
               <button
-                className="bg-blue-500 text-white text-xl font-extrabold p-5 rounded-lg"
+                className="bg-blue-700 text-white text-xl font-extrabold p-5 rounded-lg"
                 onClick={handleCancel}
               >
                 {isUploadSuccessful ? "戻る" : "キャンセル"}
               </button>
               {!isUploadSuccessful && (
                 <button
-                  className="bg-blue-500 text-white text-xl font-extrabold p-5 rounded-lg"
+                  className="bg-blue-700 text-white text-xl font-extrabold p-5 rounded-lg"
                   onClick={handleSendImage}
                   disabled={isUploading}
                 >
@@ -105,51 +106,63 @@ function MyDropzone() {
 
 export default function Home() {
   return (
-    <div className="flex flex-col m-4 items-center justify-center">
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-20 mx-auto">
-          <h1 className="sm:text-3xl text-2xl font-black title-font text-center text-gray-900 mb-20">画像マスクくん</h1>
-          <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0">
-            <div className="p-4 md:w-1/3 max-w-[306px] flex">
-              <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-[#FF9501] text-white mb-4 flex-shrink-0">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                </svg>
+    <div className="flex  items-center min-h-screen justify-center">
+      <section className="text-white body-font">
+        <div className=" container  px-5 py-20 mx-auto max-w-7xl ">
+          <div
+            className="p-[4rem]  bg-[#818181] rounded-3xl neumorphism-container"
+            style={{
+              boxShadow: `
+                inset -5px -5px 4px rgba(0, 0, 0, 0.2), 
+                inset 5px 5px 4px rgba(0, 0, 0, 0.2), 
+                0px 0px 60px rgba(0, 0, 0, 0.3)
+              `,
+              }}>
+            <div className="absolute inset-0 bg-[#a6a6a6] mix-blend-color-burn pointer-events-none"/>
+            <section className="relative mb-10">
+              <h1 className="sm:text-3xl text-2xl text-white font-black text-center mb-20">
+                顔マスク.js
+              </h1>
+              <div className="flex flex-wrap m-4 -mt-8">
+                <div className="p-4 md:w-1/3 flex">
+                  <CloudUpload  height={50} width={40} color="#fff" />
+                  <div className="flex-grow pl-6">
+                    <h2 className="text-white  text-lg font-bold mb-2">
+                      画像をドロップ
+                    </h2>
+                    <p className="leading-relaxed text-white  text-base">
+                      PNG、JPEG、JPGのみ
+                    </p>
+                  </div>
+                </div>
+                <div className="p-4 md:w-1/3 flex">
+                  <MediaImage height={50} width={40} color="#fff" />
+                  <div className="flex-grow pl-6">
+                    <h2 className="text-white text-lg font-bold mb-2">
+                      顔を検出
+                    </h2>
+                    <p className="leading-relaxed text-base">
+                      顔の座標を検出します
+                    </p>
+                  </div>
+                </div>
+                <div className="p-4 md:w-1/3 flex">
+                  <Check height={50} width={40} color="#fff" />
+                  <div className="flex-grow pl-6">
+                    <h2 className="text-white text-lg font-bold mb-2">
+                      マスク完了
+                    </h2>
+                    <p className="leading-relaxed text-base">
+                      顔にスタンプを貼ります
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className="flex-grow pl-6">
-                <h2 className="text-gray-900 text-lg title-font font-bold mb-2">画像をドロップ</h2>
-                <p className="leading-relaxed text-base">画像ファイルを枠の中にドロップorファイルから参照する</p>
-              </div>
-            </div>
-            <div className="p-4 md:w-1/3 flex">
-              <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-[#FF9501] text-white mb-4 flex-shrink-0">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
-                  <circle cx="6" cy="6" r="3"></circle>
-                  <circle cx="6" cy="18" r="3"></circle>
-                  <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-                </svg>
-              </div>
-              <div className="flex-grow pl-6">
-                <h2 className="text-gray-900 text-lg title-font font-bold mb-2">ボタンをクリック</h2>
-                <p className="leading-relaxed text-base">アップロードボタンをクリック</p>
-              </div>
-            </div>
-            <div className="p-4 md:w-1/3 flex">
-              <div className="w-12 h-12 inline-flex items-center justify-center rounded-full  bg-[#FF9501] text-white mb-4 flex-shrink-0">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
-                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                  <circle cx="12" cy="7" r="4"></circle>
-                </svg>
-              </div>
-              <div className="flex-grow pl-6">
-                <h2 className="text-gray-900 text-lg title-font font-bold mb-2">成功</h2>
-                <p className="leading-relaxed text-base">顔がマスクされた画像に変換されるよ！</p>
-              </div>
-            </div>
+            </section>
+          <MyDropzone/>
           </div>
         </div>
       </section>
-      <MyDropzone/>
     </div>
   );
 }
